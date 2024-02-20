877928df06150851b9e1f7031ede1f51be7b39ad
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2024-02-19 20:24:16 -0500

    Update page content and add sample routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let routes = [
+		{ name: 'Sample 1', path: '/sample1' },
+		{ name: 'Sample 2', path: '/sample2' },
+		{ name: 'Sample 3', path: '/sample3' }
+	];
+</script>
+
+<h1>Welcome to Zen-Mandala!</h1>
+<p>This website is currently under development, find the samples listed below:</p>
+
+<ul>
+	{#each routes as route}
+		<li><a href={route.path}>{route.name}</a></li>
+	{/each}
+</ul>
